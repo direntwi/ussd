0dc15e61216a17fefb093db5bcf478fca576d7f0
Yoocee Ansah <yooceeansah@gmail.com>
2024-10-06 16:54:01 +0100

    fixed url error issue

--- ussd/urls.py
+++ ussd/urls.py
@@ -1,6 +1,9 @@
-from django.conf.urls import url
+# from django.conf.urls import url
+
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    re_path(r"^$", views.index, name="index"),
+    # path('index/', views.index, name="index")
 ]
